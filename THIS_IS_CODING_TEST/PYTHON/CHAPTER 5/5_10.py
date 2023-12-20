ca1c7961a38c288cb8f470a5cef7b6ed2bb30f00
KIMSUNGHWAN <publicksh@gmail.com>
2023-12-21 01:05:03 +0900

    5_10.py

--- THIS_IS_CODING_TEST/PYTHON/CHAPTER 5/5_10.py
+++ THIS_IS_CODING_TEST/PYTHON/CHAPTER 5/5_10.py
@@ -1,10 +1,36 @@
 import sys
-sys.stdin = open("TEST_TXT/5_10_1.txt", "r")
+sys.stdin = open("TEST_TXT/5_10_2.txt", "r")
 
 n, m = map(int, input().split())
 
-array = []
-for i in range(n):
-    array.append(list(map(int, input())))
+count = 0
 
-print(array)
+graph = []
+
+def dfs(x,y):
+    if x <= -1 or y <= -1 or x >= n or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x,y-1)
+        dfs(x,y+1)
+        return True
+    return False
+# def dfs(graph, v, visited):
+#     visited[v] = True
+#     print(v, end=' ')
+#     for i in graph[v]:
+#         if not visited[i]:
+#             dfs(graph, i, visited)
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j):
+            count += 1
+
+print(count)
+
