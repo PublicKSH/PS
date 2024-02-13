45f197d1a4d7150184d6016b206d88cf11b9a1fd
KIMSUNGHWAN <publicksh@gmail.com>
2024-02-14 00:06:11 +0900

    0213

--- THIS_IS_CODING_TEST/PYTHON/PART3/38.py
+++ THIS_IS_CODING_TEST/PYTHON/PART3/38.py
@@ -1,7 +1,5 @@
-# 답은 맞았는데 출력형식 안바꿈
-n = int(input())
-m = int(input())
-INF = 1e9
+n,m = map(int, input().split())
+INF = 9
 mapList = [[INF] * (n) for _ in range(n)]
 
 
@@ -11,9 +9,9 @@ for i in range(n):
             mapList[i][j] = 0
 
 for i in range(m):
-    a, b, c = map(int, input().split())
-    if mapList[a-1][b-1] > c:
-        mapList[a-1][b-1] = c
+    a, b = map(int, input().split())
+    mapList[a-1][b-1] = 1
+
 
 for k in range(n):
     for i in range(n):
@@ -23,19 +21,3 @@ for k in range(n):
 
 for i in range(n):
     print(mapList[i])
-# 5
-# 14
-# 1 2 2
-# 1 3 3 
-# 1 4 1
-# 1 5 10
-# 2 4 2
-# 3 4 1
-# 3 5 1
-# 4 5 3
-# 3 5 10
-# 3 1 8
-# 1 4 2
-# 5 1 7
-# 3 4 2
-# 5 2 4
