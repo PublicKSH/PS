8faddc1ff54ee0622adf20620eb386d70986388b
KIMSUNGHWAN <publicksh@gmail.com>
2024-02-26 23:23:52 +0900

    '0226'

--- BJ/11000/11052.py
+++ BJ/11000/11052.py
@@ -1,7 +1,11 @@
-# n = int(input())
-# packArr = list(map(int, input().split()))
-# dp = [0] * (n)
-# for i in range(len(packArr)):
-#     print(i, packArr[i])
-#     for j in range(i, len(dp)-1):
-#         # dp[j] = max(dp[j])
+n = int(input())
+packArr = list(map(int, input().split()))
+dp = [0] * n
+for i in range(n):
+    dp[i] = packArr[0] * (i+1)
+for i in range(1, len(packArr)):
+    dp[i] = max(packArr[i], dp[i])
+    for j in range(i+1, n):
+        dp[j] = max(dp[j], dp[j-i-1] + dp[i])
+
+print(dp[n-1])
