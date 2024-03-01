4b11d2df40c7caa3482aa178e50272d68945ae4e
KIMSUNGHWAN <publicksh@gmail.com>
2024-03-01 19:56:30 +0900

    0301

--- BJ/02000/2293.py
+++ BJ/02000/2293.py
@@ -1,16 +1,13 @@
-# todo: 다시풀기
 n, k = map(int, input().split())
 coinList = []
-dp = [0] * (k+1)
-dp[0] = 1
 for i in range(n):
-    coin = int(input())
-    coinList.append(coin)
+    coinList.append(int(input()))
+dp = [0 for _ in range(k+1)]
+# coin 만 써서 만드는 경우의 수
+dp[0] = 1
 coinList.sort()
-
 for coin in coinList:
-    for j in range(coin, k+1):
-        print(j, j-coin)
-        dp[j] += dp[j-coin]
+    for i in range(coin, k+1):
+        dp[i] = dp[i-coin] + dp[i]
 
-print(dp)
+print(dp[k])
