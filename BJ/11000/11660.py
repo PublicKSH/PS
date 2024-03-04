ba9868066efd2610974a58b368c24ceb20dce901
KIMSUNGHWAN <publicksh@gmail.com>
2024-03-04 22:15:54 +0900

    segment tree

---
+++ BJ/11000/11660.py
@@ -0,0 +1,16 @@
+# 세그먼트 트리
+n, m = map(int, input().split())
+
+arr = []
+sum = 0
+for i in range(n):
+    inputArr = list(map(int, input().split()))
+    for num in inputArr:
+        print(num)
+
+print(arr)
+arrXY = []
+for i in range(m):
+    arrXY.append(list(map(int, input().split())))
+
+print(arrXY)
