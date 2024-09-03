a6cf17b7cf1bcfa6a1bc3b7e7b167eebebfc8d75
PublicKSH <publicksh@gmail.com>
2024-09-04 00:49:27 +0900

    1d1s

--- BJ/11000/11660.py
+++ BJ/11000/11660.py
@@ -1,16 +1,23 @@
-# 세그먼트 트리
+# 세그먼트 트리 로도 풀어보자
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 
 arr = []
-sum = 0
+
 for i in range(n):
-    inputArr = list(map(int, input().split()))
-    for num in inputArr:
-        print(num)
+    subArray = list(map(int, input().split()))
+    arr.append(subArray)
 
-print(arr)
-arrXY = []
-for i in range(m):
-    arrXY.append(list(map(int, input().split())))
+sumArr = [[0]*(n+1) for i in range(n+1)]
 
-print(arrXY)
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        sumArr[i][j] = sumArr[i][j-1] + sumArr[i-1][j] - sumArr[i-1][j-1] + arr[i-1][j-1]
+        
+for k in range(m):
+    x1,y1,x2,y2 = map(int,input().split())
+    
+    result = sumArr[x2][y2] - sumArr[x2][y1-1] -sumArr[x1-1][y2] + sumArr[x1-1][y1-1]
+    print(result)
