8504b4caaf865ca6847542fc90635d3423d00ddc
PublicKSH <80028092+PublicKSH@users.noreply.github.com>
2025-04-07 17:55:13 +0900

    [Silver I] Title: 정수 삼각형, Time: 44 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int arr[501][501];
+int dp[501][501] = {0};
+ 
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    dp[1][1] = arr[1][1];
+    for(int i = 2; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j];
+        }
+    }
+    int max = 0;
+    for(int i = 1; i <= n; i++){
+        if(max < dp[n][i])
+            max = dp[n][i];
+    }
+    
+    cout << max << '\n';
+    
+    return 0;
+}
+
