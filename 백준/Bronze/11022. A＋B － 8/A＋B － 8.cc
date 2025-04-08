368443875cc519ac89e0ee4b45477fbcca853855
PublicKSH <80028092+PublicKSH@users.noreply.github.com>
2025-04-08 11:39:09 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int a,b;
+	cin >> n;
+	for (int i=1;i<n+1; i++)
+	{
+		cin >> a >> b;
+		cout <<"Case #"<< i <<": " <<a<<" + " << b <<" = "<< a+b <<endl;
+	}
+	return 0;
+}
