c22b0ac904da9384011813c0e7758c3cba3f2f1a
PublicKSH <80028092+PublicKSH@users.noreply.github.com>
2025-04-08 11:38:27 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	while(a+b != 0)
+	{
+		cin >> a>> b;
+		if (a==0 && b==0) break;
+		cout << a+b << endl;
+	}
+
+	return 0;
+}
