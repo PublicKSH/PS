d8f3dbd396863919130e1499c98b71229a8d8231
PublicKSH <80028092+PublicKSH@users.noreply.github.com>
2025-04-08 11:43:22 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,17 @@
+# 1. dfs
+n, m = map(int, input().split())
+arr = [i for i in range(1, n+1)]
+resultArr = [0] * (m+1)
+
+def dfs(l, s):
+    if l == m:
+        for i in range(m):
+            print(resultArr[i], end=' ')
+        print()
+    else:
+        for i in range(s, n):
+            resultArr[l] = arr[i]
+            dfs(l+1, i+1)
+
+dfs(0,0)
+# 2. 모듈
