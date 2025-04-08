7ea149e27e0cee3dd393789e515576f0f8e57353
PublicKSH <80028092+PublicKSH@users.noreply.github.com>
2025-04-08 11:43:17 +0900

    [Silver III] Title: N과 M (1), Time: 236 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,18 @@
+# 1. dfs
+n, m = map(int, input().split())
+arr = [0] * (n + 1)
+visited = [False] * (n + 1)
+def dfs(count):
+    if count == m:
+        for i in range(m):
+            print(arr[i], end=' ')
+        print()
+    else:
+        for i in range(1, n+1):
+            if not visited[i] :
+                visited[i] = True
+                arr[count] = i
+                dfs(count+1)
+                visited[i] = False
+
+dfs(0)
