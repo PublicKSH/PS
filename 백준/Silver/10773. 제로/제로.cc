47ad178b060922eacd09b917f9e5f5a1ee4049ed
PublicKSH <80028092+PublicKSH@users.noreply.github.com>
2025-04-08 11:36:32 +0900

    [Silver IV] Title: 제로, Time: 24 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,25 @@
+//실버4 제로
+//문제를 보자마자 스텍을 사용해야 겟다고 생각
+#include <bits/stdc++.h>
+using namespace std;
+
+int k;
+int sum = 0;
+stack<int> numbers;
+int main() {
+    cin >> k;
+    for (int i=0; i<k; i++) {
+        int x;
+        cin >> x;
+        if (x == 0)
+            numbers.pop();
+        else
+            numbers.push(x);
+    }
+
+    while(!numbers.empty()) {
+        sum += numbers.top();
+        numbers.pop();
+    }
+    cout << sum;
+}
